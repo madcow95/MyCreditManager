3ba16177281d04481a038f8975ebc64f720458b2
MadCow <78129823+madcow95@users.noreply.github.com>
2023-05-06 01:45:22 +0900

    사전과제 SOLID로 재시도 2

--- MyCreditManagerSOLID/MyCreditManagerSOLID/main.swift
+++ MyCreditManagerSOLID/MyCreditManagerSOLID/main.swift
@@ -7,19 +7,56 @@
 
 import Foundation
 
-enum CreditMode {
-    case addStudent
-    case removeStudent
-    case addOrChangeScore
-    case removeScore
-    case checkScore
+
+protocol AddStudent {
+    func addStudent()
+}
+
+protocol RemoveStudent {
+    func removeStudent()
+}
+
+protocol AddOrChangeScore {
+    func addOrChangeScore()
+}
+
+protocol RemoveScore {
+    func removeScore()
+}
+
+protocol CheckScore {
+    func checkScore()
+}
+
+class CreditMode: AddStudent, RemoveStudent, AddOrChangeScore, RemoveScore, CheckScore {
+    func addStudent() {
+        print("add Student")
+    }
+    
+    func removeStudent() {
+        print("remove Student")
+    }
+    
+    func removeScore() {
+        print("remove Score")
+    }
+    
+    func addOrChangeScore() {
+        print("add or change score")
+    }
+    
+    func checkScore() {
+        print("check score")
+    }
+    
+    
 }
 
 class CreditManager {
     
-    let mode: CreditMode
+    let mode: String
     
-    init(mode: CreditMode) {
+    init(mode: String) {
         self.mode = mode
     }
     
@@ -28,7 +65,12 @@ class CreditManager {
     }
 }
 
-let enteredKey = readLine()!
-if !["1", "2", "3", "4", "5", "x", "X"].contains(enteredKey) {
-    print("뭔가 입력이 잘못되었습니다. 1~5 사이의 숫자 혹은 X를 입력해주세요.")
-}
+//let enteredKey = readLine()!
+//if !["1", "2", "3", "4", "5", "x", "X"].contains(enteredKey) {
+//    print("뭔가 입력이 잘못되었습니다. 1~5 사이의 숫자 혹은 X를 입력해주세요.")
+//}
+
+let creditManager = CreditManager(mode: "")
+creditManager.initializeManager()
+let creditMode = CreditMode()
+creditMode.addOrChangeScore()
